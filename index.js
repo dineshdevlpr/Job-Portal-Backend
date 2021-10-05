3dac9f2cfb785777442204cfab10eb8a59e5d6d1
dineshdevlpr <dineshiaf30@gmail.com>
2021-10-05 06:06:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require("cors");
+require("dotenv").config();
+
+// auth routes
+const authRecruiter = require("./src/auth-routes/authRecruiter");
+const authCandidate = require("./src/auth-routes/authCandidate")
+
+// authentication middleware
+// const isRecruiter = require("./src/middleware/isRecruiter");
+// const isCandidate = require("./src/middleware/isCandidate")
+
+// job portal routes
+const recruiterPortal = require("./src/job-routes/recruiterPortal")
+
+const app = express()
+
+const port = process.env.PORT || 4000;
+const dbUrl = process.env.DB_URL;
+
+app.use(express.json())
+app.use(cors({
+    origin : "*",
+    credentials : true
+}));
+app.use("/recruiter", authRecruiter)
+app.use("/candidate", authCandidate)
+app.use("/recruiter-portal", recruiterPortal)
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`)
+})
---
+++ src/job-routes/recruiterPortal.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const router = require("express").Router();
+const { MongoClient, ObjectID } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const isRecruiter = require("../middleware/isRecruiter");
+
+
+const dbUrl = process.env.DB_URL;
+const app = express()
+app.use(cors({
+    origin : "*",
+    credentials : true
+}));
+
+// create jobs
+
+router.post('/createjob', isRecruiter, async (req, res) => {
+
+    try {
+        let client = await MongoClient.connect(dbUrl);
+        let db = client.db("Job-Portal");
+        let jobId = await db.collection("jobs").findOne({ jobId : req.body.jobId })
+        if (!jobId) {
+            let data = await db.collection("jobs").insertOne({ jobId : req.body.jobId, description : req.body.description , skills : req.body.skills , date : new Date().toLocaleDateString()});
+            res.status("200").json({ message: "Job Created successfully" })
+        } else {
+            res.status("401").json({ message: "Job ID Already Exists" })
+        }
+        client.close();
+    } catch (error) {
+        console.log(error)
+        res.status(500)
+    }
+})
+
+// list of jobs created
+
+router.get('/listjobs', isRecruiter, async (req, res) => {
+    try {
+        let client = await MongoClient.connect(dbUrl);
+        let db = client.db("Job-Portal");
+        let data = await db.collection("jobs").find().toArray();
+        if (data) {
+            res.status(200).json(data);
+        } else {
+            res.status(404).json({ message: "No Data Found" })
+        }
+        client.close();
+    } catch (error) {
+        console.log(error)
+        res.status(500)
+    }
+
+})
+
+
+
+module.exports = router;
